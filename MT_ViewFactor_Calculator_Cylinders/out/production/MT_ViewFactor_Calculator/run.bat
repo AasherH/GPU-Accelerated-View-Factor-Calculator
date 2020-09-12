20150b4dc52d76e098bb80302aefe74ac9d8e498
Asher Hancock <51274457+AasherH@users.noreply.github.com>
2020-09-12 19:13:06 -0400

    Add files via upload

---
+++ MT_ViewFactor_Calculator_Cylinders/out/production/MT_ViewFactor_Calculator/run.bat
@@ -0,0 +1,4 @@
+SET CLASSPATH=.;C:\Users\ajh172\Desktop\MT_ViewFactor_Calculator\out\production\MT_ViewFactor_Calculator
+C:\Users\ajh172\Desktop\MT_ViewFactor_Calculator\src\aparapi-1.3.4.jar
+C:\Users\ajh172\Desktop\MT_ViewFactor_Calculator\src\j3d-org-all_1.1.0.jar
+C:\Users\ajh172\Desktop\MT_ViewFactor_Calculator\src\aparapi-jni-1.1.2.jar TEDServer
